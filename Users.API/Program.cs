2e3a2617de437e980de838a9a77fc3d683317fee
Kevin Baquero <baquerochavarro@gmail.com>
2023-04-04 16:48:35 -0500

    Adding Middleware for handle errors

--- Users.API/Program.cs
+++ Users.API/Program.cs
@@ -1,3 +1,13 @@
+using AutoMapper.Data;
+using Users.API.AutoMapper;
+using Users.API.Middlewares;
+using Users.Business.Gateway;
+using Users.Business.Gateway.Repositories;
+using Users.Business.UseCases;
+using Users.Infrastructure;
+using Users.Infrastructure.Interfaces;
+using Users.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(config => config.AddDataReaderMapping(), typeof(ConfigurationProfile));
+
+builder.Services.AddScoped<IWorkTeamUseCase, WorkTeamUseCase>();
+builder.Services.AddScoped<IWorkTeamRepository, WorkTeamRepository>();
+
+builder.Services.AddSingleton<IContext>(provider => new Context(builder.Configuration.GetConnectionString("urlConnection"), "TrackingProjectManager"));
 
 var app = builder.Build();
 
@@ -19,6 +35,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ErrorHandleMiddleware>();
 
 app.MapControllers();
 
