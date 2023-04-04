272d3c7a4a70bf8e5936e5bffe692e2e3c7e2a41
Kevin Baquero <baquerochavarro@gmail.com>
2023-04-04 16:49:29 -0500

    Configure context for DB

---
+++ Users.Infrastructure/Interfaces/IContext.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+using Users.Infrastructure.Entities;
+
+namespace Users.Infrastructure.Interfaces
+{
+    public interface IContext
+    {
+        public IMongoCollection<UserMongo> Users { get; }
+        public IMongoCollection<WorkTeamMongo> WorkTeams { get; }
+    }
+}
